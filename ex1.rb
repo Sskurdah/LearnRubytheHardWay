636ca49a18f5e7450c29c7c554d99f8f992df601
Samantha <samantha.skurdahl@gmail.com>
2015-04-25 17:19:09 -0400

    Create ex1.rb

---
+++ ex1.rb
@@ -0,0 +1,11 @@
+Samanthas-MacBook-Pro-3:~ samanthaskurdahl$ cd desktop 
+Samanthas-MacBook-Pro-3:desktop samanthaskurdahl$ cd LearnRuby 
+Samanthas-MacBook-Pro-3:LearnRuby samanthaskurdahl$ ruby ex1.rb
+Hello World!
+Hello Again.
+I like typing this.
+This is fun.
+Yay! Printing
+I'd much rather you 'not'.
+I "said" do not touch this.
+Samanthas-MacBook-Pro-3:LearnRuby samanthaskurdahl$ 
